4f9b19a2ed61175e608e4d727be4104b38efd109
Meghana Kandru <55113576+mekandru@users.noreply.github.com>
2025-03-12 17:21:06 -0700

    Update CurrentWeather.js

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,43 +1,38 @@
-import axios from 'axios';
 import React from 'react';
 import './CurrentWeather.scss';
-class CurrentWeather extends React.Component{
-    render(){
+
+class CurrentWeather extends React.Component {
+    render() {
         let img;
-        if(this.props.icon){
-        const url=`http://openweathermap.org/img/wn/${this.props.icon}@2x.png`;
-            img=(<img className="current-weather_icon" src={url} alt={this.props.description}/>);
+        if (this.props.icon) {
+            const url = `http://openweathermap.org/img/wn/${this.props.icon}@2x.png`;
+            img = (<img className="current-weather_icon" src={url} alt={this.props.description} />);
         }
-        return(
+        return (
             <div className="current-weather">
                 <div className="current-weather_content">
                     <div className="current-weather_text">
-                   
-                      <p className="current-weather_name">Today</p>
-                      <p className="current-weather_temp">
-                     {this.props.currentTemperature}{'\u00b0'}F
+
+                        <p className="current-weather_name">Today</p>
+                        <p className="current-weather_temp">
+                            {this.props.currentTemperature}{'\u00b0'}F
                         </p>
                         <p className="current-weather_mintemp">
-                    Low :   {this.props.minimumTemp}{'\u00b0'}F 
+                            Low :   {this.props.minimumTemp}{'\u00b0'}F
                         </p>
                         <p className="current-weather_maxtemp">
-                    High:   {this.props.maximumTemp}{'\u00b0'}F 
+                            High:   {this.props.maximumTemp}{'\u00b0'}F
                         </p>
-                        </div>
-                        <div>
-                          {img}
-                        {/*                               
-                        <p className="current-weather_feels-like">
-                       Feels Like {this.props.feelsLike}
-                        </p>
-                        */}
+                    </div>
+                    <div>
+                        {img}
                         <p className="current-weather_description">
-                       {this.props.description}
+                            {this.props.description}
                         </p>
-                        </div>
-                </div> 
+                    </div>
+                </div>
             </div>
-        )
+        );
     }
 }
 
